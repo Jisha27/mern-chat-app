6c287a5ebdd3f6b175c5fae7ba7522b536008d0c
Jisha27 <jishasumesh27@gmail.com>
2026-05-20 11:08:18 +0400

    fixed a production function

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,11 +43,14 @@ app.use("/api/message",messageRoutes)
 // Express serves dist folder
 //         ↓
 // App works from one server
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
   });
 }
 
