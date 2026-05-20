9d468cb1d3263044981e50cf613be86e2499fbd3
Jisha27 <jishasumesh27@gmail.com>
2026-05-20 11:18:55 +0400

    fixed  a typo in default theme

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "Autumn",
+  theme: localStorage.getItem("chat-theme") || "autumn",
   setTheme: (theme) => {
     localStorage.setItem("chat-theme", theme);
     set({ theme });
